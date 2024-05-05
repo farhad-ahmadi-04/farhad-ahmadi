c74c91fc0066eb37f68cbd131451e0c2fb998443
farhad-ahmadi-04 <farhad.13ahmadi.83@mail.com>
2024-05-05 12:42:53 +0430

    feat: create hero section

---
+++ src/js/components/hero.js
@@ -0,0 +1,35 @@
+import domGenerator from "../../../node_modules/dom-generator/dist/domGenerator.js";
+
+const heroSection = () => {
+    return domGenerator({
+        tag: "section",
+        attributes: { class: "hero-section" },
+        children: [{
+            tag: "div",
+            attributes: { class: "hero-item" },
+            children: [
+                {
+                    tag: "p",
+                    properties: { textContent: "Hi I am" }
+                }, {
+                    tag: "h3",
+                    attributes: { class: "hero-name" },
+                    properties: { textContent: "Farhad Ahmadi" },
+                }, {
+                    tag: "h1",
+                    attributes: { class: "hero-skill" },
+                    properties: { textContent: "Front-end dev" },
+                }, {
+                    tag: "div",
+                    attributes: { class: "hero-media" },
+                    children: [{
+                        tag: iconBtn("instagram"),
+                    }, {
+                        tag: iconBtn("github"),
+                    }, {
+                        tag: iconBtn("lonkdin"),
+                    }],
+                }],
+        }]
+    })
+};
