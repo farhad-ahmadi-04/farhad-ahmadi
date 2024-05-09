3165e9d079d1ccb05ade9df97af4446a1669d0ab
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-09 15:26:03 +0430

    feat: create about me section

--- src/js/components/aboutMe.js
+++ src/js/components/aboutMe.js
@@ -1,1 +1,11 @@
 import domGenerator from "../../../node_modules/dom-generator/dist/domGenerator.js";
+import label from "./lable.js";
+
+const aboutMESection = () => {
+    return domGenerator({
+        tag: 'section',
+        children: [{
+            tag: label("About me", "front end developer and also UI/UX designer")
+        }]
+    })
+}
