7914a779392e0f76b6c3557f16953a0127ed1e5e
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-13 16:31:16 +0430

    feat: create textIconBtn

--- src/js/components/textIconBtn.js
+++ src/js/components/textIconBtn.js
@@ -1,1 +1,21 @@
 import domGenerator from "../../../node_modules/dom-generator/dist/domGenerator.js";
+
+const textIconBtn = (url, image, alt, text) => {
+    return domGenerator({
+        tag: "a",
+        attributes: { class: "text-icon-btn", href: url },
+        children: [{
+            tag: 'button',
+            children: [{
+                tag: "img",
+                attributes: { class: "icon-btn-image" },
+                properties: { src: image, alt: alt }
+            }, {
+                tag: "span",
+                attributes: { class: "primary--text-btn" },
+                properties: { textContent: text }
+            }]
+        }]
+    })
+}
+export default textIconBtn;
