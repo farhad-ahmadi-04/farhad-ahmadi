80e17116aa17268f9d8676286feac59b125c71ae
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-15 21:48:40 +0430

    feat: set same categries to the same array + get render from categry btn

--- src/js/components/main/portfolio/portfolio.js
+++ src/js/components/main/portfolio/portfolio.js
@@ -1,11 +1,27 @@
 import generateElement from "../../../Assets/generateEle.js";
 import render from "../../../Assets/render.js";
+import textBtn from "../../textBtn.js";
 import portfolioLabel from "./portfolioLabel.js";
 // import textBtn from "../../textBtn.js";
 
 const portfolio = (data) => {
     const portfolioSec = document.querySelector(".portfolio-sec")
     render(portfolioSec, portfolioLabel("Portfolio"))
+
+    const category = document.querySelector(".category")
+
+    // find categoies
+    const uniqueCategory = [...new Set(data.portfolio.map(item => item.category))];
+    // set same category to same array
+    const groupedPeople = uniqueCategory.map(category => {
+        return data.portfolio.filter(project => project.category === category);
+    });
+
+    // get render fron categories
+    uniqueCategory.map(item => render(category, textBtn(item, "#")))
+
+
+
 
 }
 export default portfolio
