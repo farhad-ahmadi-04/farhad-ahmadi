3134e65fd94eb862c214ee946402f06a45713327
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-15 14:43:45 +0430

    feat: create skill card component

--- src/js/components/skillCard.js
+++ src/js/components/skillCard.js
@@ -1,1 +1,39 @@
 import domGenerator from "../../../node_modules/dom-generator/dist/domGenerator.js";
+
+/**
+ * create skill card component with dom generator 
+ * @param {object} data - data of crad(image, alt, texts) from json
+ * @returns html element
+ */
+const skillCard = (data) => {
+    return domGenerator({
+        tag: "div",
+        attributes: { class: "skillCard" },
+        children: [{
+            tag: "div",
+            attributes: { class: "progress" },
+            children: [{
+                tag: "div",
+                attributes: { class: "progress-bar" },
+                children: [{
+                    tag: "image",
+                    attributes: { class: "skill-icon" },
+                    properties: { src: data.img, alt: data.alt },
+                }]
+            }]
+        }, {
+            tag: "div",
+            attributes: { class: "skill" },
+            children: [{
+                tag: "span",
+                attributes: { class: "progress-range" },
+                properties: { textContent: `${data.avrage}%` }
+            }, {
+                tag: "span",
+                attributes: { class: "skill-item" },
+                properties: { textContent: data.tool }
+            }]
+        }]
+    })
+}
+export default skillCard
