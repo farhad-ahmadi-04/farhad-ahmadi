cf20fad749094c6fb77acdc836766711b7cf62db
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-17 01:03:26 +0430

    feat: create portfolio card component & add comment to function

--- src/js/components/main/portfolio/portfolio.js
+++ src/js/components/main/portfolio/portfolio.js
@@ -1,9 +1,15 @@
-import generateElement from "../../../Assets/generateEle.js";
 import render from "../../../Assets/render.js";
 import textBtn from "../../textBtn.js";
 import portfolioLabel from "./portfolioLabel.js";
 // import textBtn from "../../textBtn.js";
 
+/**
+ * create portfoliosection and add component to the section:
+ * 1st: get render from label 
+ * 2nd: find categories from object and push it to array
+ * 3rd: get render from categories by map method
+ * @param {object} data data from json file
+ */
 const portfolio = (data) => {
     const portfolioSec = document.querySelector(".portfolio-sec")
     render(portfolioSec, portfolioLabel("Portfolio"))
@@ -12,7 +18,7 @@ const portfolio = (data) => {
 
     // find categoies
     const uniqueCategory = Object.keys(data.portfolio)
-    // get render fron categories
+    // get render from categories
     render(category, textBtn("All", "primary--text-btn"))
     uniqueCategory.map(item => render(category, textBtn(item, "secondary--text-btn")))
 
--- src/js/components/main/portfolio/portfolioCrad.js
+++ src/js/components/main/portfolio/portfolioCrad.js
@@ -1,9 +1,45 @@
 import domGenerator from "../../../../../node_modules/dom-generator/dist/domGenerator.js";
+import textIconBtn from "../../textIconBtn.js";
 
+/**
+ * create card component with dom generator
+ * @param {object} data - data for create card
+ * @returns html element(card)
+ */
 const portfolioCard = (data) => {
     return domGenerator({
         tag: 'div',
-        attributes: { class: "portfolioCard" },
+        attributes: { class: "portfolio-Card" },
+        children: [{
+            tag: 'div',
+            attributes: { class: "portfolio-head" },
+            children: [{
+                tag: 'img',
+                properties: { src: "#", alt: "farhad" }
+            }]
+        }, {
+            tag: 'div',
+            attributes: { class: "portfolio-body" },
+            children: [{
+                tag: 'div',
+                attributes: { class: "portfolio-body-label" },
+                children: [{
+                    tag: 'span',
+                    attributes: { class: "portfolio-name" },
+                    properties: { textContent: "Name Project" },
+                }, {
+                    tag: 'span',
+                    attributes: { class: "portfolio-category" },
+                    properties: { textContent: "category" },
+                }]
+            }, {
+                tag: 'p',
+                attributes: { class: "portfolio-body-description" },
+                properties: { textContent: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups." },
+            }, {
+                tag: textIconBtn(data.icon.arrowRight, "Arrow-Right", "source code", "primary--textIcon-btn"),
+            }]
+        }]
     })
 }
 export default portfolioCard
