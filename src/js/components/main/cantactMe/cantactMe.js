230d3de1b954d4611cd8f18991fc1c276bfc0981
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-17 20:00:03 +0430

    feat:get render form form & select it

--- src/js/components/main/cantactMe/cantactMe.js
+++ src/js/components/main/cantactMe/cantactMe.js
@@ -1,13 +1,18 @@
 import render from "../../../Assets/render.js";
 import label from "../../lable.js";
+import contactForm from "./contactForm.js";
 
 /**
  * create contact me section with all nessesaries components:
  * 1st: selct section & get render of label
+ * 2nd: get render form form & select it
  */
 const cantactMe = () => {
     // 1st step:
     const cantactSec = document.querySelector('.cantact-me-sec')
     render(cantactSec, label("Contact Me", "Cultivating Connections: Reach Out and Connect with Me"))
+    // 2nd dtep :
+    render(cantactSec, contactForm())
+    const contactForm = document.querySelector("#contact-form")
 }
 export default cantactMe
