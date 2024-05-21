456f74863aec4c01089640bd66804bef34827e41
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-21 13:29:43 +0430

    feat: get render from btn for submit form

--- src/js/components/main/cantactMe/cantactMe.js
+++ src/js/components/main/cantactMe/cantactMe.js
@@ -1,13 +1,15 @@
 import render from "../../../Assets/render.js";
 import label from "../../lable.js";
 import contactForm from "./contactForm.js";
+import textBtn from "../../buttons/textBtn.js";
 import { inputCom, textareaCom } from "./contactInput.js";
 
 /**
  * create contact me section with all nessesaries components:
  * 1st: selct section & get render of label
  * 2nd: get render form form & select it
- * 3rd: getrender from inputs
+ * 3rd: get render from inputs
+ * 4rd: select div of sub,it button & get render from button
  */
 const cantactMe = () => {
     // 1st step:
@@ -16,12 +18,15 @@ const cantactMe = () => {
     // 2nd dtep :
     render(cantactSec, contactForm())
     const Form = document.querySelector("#contact-form")
-    const inputForm = document.querySelector(".submit-div")
+    const inputForm = document.querySelector(".inputs")
     // 3th step:
     render(inputForm, inputCom("input", "text", "user-name", "Name", undefined))
     render(inputForm, inputCom("input", "email", "user-email", "Email", undefined))
     render(inputForm, inputCom("input", "text", "user-email", "Email", undefined))
     render(inputForm, textareaCom("textarea", "Message", undefined))
+    // 4th step:
+    const submitFormBtn = document.querySelector(".submit-div")
+    render(submitFormBtn, textBtn("Send", "outline-primary--text-btn"))
 
 }
 export default cantactMe
