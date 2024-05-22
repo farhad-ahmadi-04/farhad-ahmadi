363d4b0f2506689e7ee90f40c493ddb50ad598eb
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-22 13:44:44 +0430

    feat: set data about me to json

--- src/js/components/footer/footer.js
+++ src/js/components/footer/footer.js
@@ -1,6 +1,7 @@
 import fetchData from "../../Assets/fetchData.js"
 import render from "../../Assets/render.js"
 import iconBtn from "../buttons/iconbtn.js"
+import textIconBtn from "../buttons/textIconBtn.js"
 import nav from "../nav.js"
 import footerGen from "./footerGen.js"
 
@@ -13,6 +14,7 @@ const obj = fetchData("src/js/Assets/data.json")
  * 1st: generate assets stracture of footer
  * 2nd: select nav div & getrender from navi in footer
  * 3rd: select media div & get data from json and loop on that then pass it to media function.
+ * 4th: select contact-footer div & grt rendr from data to this div...:)
  * @param {HTMLElement} app - html element to get render 
  */
 const footer = (app) => {
@@ -28,6 +30,8 @@ const footer = (app) => {
     obj.then(data => media(data.socialMedia, mediaFooter)).
         catch(err => console.error(err))
 
+    // 4th step:
+
 }
 
 /**
@@ -37,7 +41,5 @@ const footer = (app) => {
  */
 function media(media, pos) {
     media.map(item => render(pos, iconBtn(item.icon, item.alt, item.address, "icon-btn")))
-
-
 }
 export default footer
