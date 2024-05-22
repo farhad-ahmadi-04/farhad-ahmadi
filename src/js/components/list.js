b0b518c48e93734da6b8f0161f34f60ddf6726d8
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-22 16:52:49 +0430

    direction: change direction & rename header to list.js

---
+++ src/js/components/list.js
@@ -0,0 +1,23 @@
+const liList = [
+    {
+        item: "home",
+        link: "#",
+        class: "active"
+    }, {
+        item: "Services",
+        link: "#",
+        class: "primary--text"
+    }, {
+        item: "About me",
+        link: "#",
+        class: "primary--text"
+    }, {
+        item: "Portfolio",
+        link: "#",
+        class: "primary--text"
+    }, {
+        item: "Contact me",
+        link: "#",
+        class: "primary--text"
+    }
+]
