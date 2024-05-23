fb781a2c5945fdcd97554f4aba0ed0d3d83be7da
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-24 01:13:20 +0430

    feat: create strcture of mobile nav js

--- src/js/components/nav/mobileNav.js
+++
@@ -1,1 +0,0 @@
-import domGenerator from "../../../../node_modules/dom-generator/dist/domGenerator.js";
---
+++ src/js/components/nav/mobileNavGen.js
@@ -0,0 +1,34 @@
+import domGenerator from "dom-generator/dist/domGenerator.js";
+
+const mobileNav = (logo) => {
+    domGenerator({
+        tag: 'div',
+        attributes: { class: 'mobile-nav' },
+        children: [{
+            tag: 'div',
+            attributes: { class: 'mobile-nav-head' },
+            children: [{
+                tag: 'h1',
+                attributes: { class: 'logo' },
+                properties: { textContent: logo }
+            }, {
+                tag: 'div',
+                attributes: { class: 'colse-mobile-nav' },
+                children: [{
+                    tag: 'span',
+                    attributes: { class: 'line btn-top-line' }
+                }, {
+                    tag: 'span',
+                    attributes: { class: 'line btn-bottom-line' }
+                }]
+            }]
+        }, {
+            tag: 'nav',
+            attributes: { class: 'mobile-nav' }
+        }, {
+            tag: 'div',
+            attributes: { class: 'hire-me' },
+        }]
+    })
+}
+export default mobileNav
