8685f88fc52c3fdedd71159a7a671675b776ab69
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-24 17:27:08 +0430

    feat: select nav & get render from ul list + style to mobile navbar

--- src/js/section/header/mobileNavbar.js
+++ src/js/section/header/mobileNavbar.js
@@ -1,13 +1,24 @@
 import render from "../../Assets/render.js";
+import { liList } from "../../components/list.js";
 import mobileNavGen from "../../components/nav/mobileNavGen.js";
+import ul from "../../components/nav/ulList.js";
 
 /**
  * create mobile navbar
  * 1st: get render for stracture
+ * 2nd: select nav & get render from ul list
  * @param {Element} pos - element for appending to
  */
 const mobileNavbar = (pos) => {
     // 1st:
     render(pos, mobileNavGen('feri'))
+
+    // 2nd:
+    const mobileNav = document.querySelector('.mobile-nav')
+    render(mobileNav, ul(liList, 'mobile-nav-ul'))
+
+
+
+
 }
 export default mobileNavbar
