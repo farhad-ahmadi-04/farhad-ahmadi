b1dc5c83250de8881db929facf959b4e8cdfc6c3
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-24 17:35:24 +0430

    feat: select hire me div & get render from text btn

--- src/js/section/header/mobileNavbar.js
+++ src/js/section/header/mobileNavbar.js
@@ -1,4 +1,5 @@
 import render from "../../Assets/render.js";
+import textBtn from "../../components/buttons/textBtn.js";
 import { liList } from "../../components/list.js";
 import mobileNavGen from "../../components/nav/mobileNavGen.js";
 import ul from "../../components/nav/ulList.js";
@@ -7,6 +8,7 @@ import ul from "../../components/nav/ulList.js";
  * create mobile navbar
  * 1st: get render for stracture
  * 2nd: select nav & get render from ul list
+ * 3nd: select hire me div & get render from text btn
  * @param {Element} pos - element for appending to
  */
 const mobileNavbar = (pos) => {
@@ -17,6 +19,9 @@ const mobileNavbar = (pos) => {
     const mobileNav = document.querySelector('.mobile-nav')
     render(mobileNav, ul(liList, 'mobile-nav-ul'))
 
+    // 3rd:
+    const hireMeBtn = document.querySelector('.hire-me')
+    render(hireMeBtn, textBtn('Hire me', 'primary--text-btn'))
 
 
 
