22e503ad4c920470b6734d45c8e6d10495fac016
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-05-24 21:01:01 +0430

    style: set mobile navbar styel

--- src/js/section/header/mobileNavbar.js
+++ src/js/section/header/mobileNavbar.js
@@ -23,7 +23,40 @@ const mobileNavbar = (pos) => {
     const hireMeBtn = document.querySelector('.hire-me')
     render(hireMeBtn, textBtn('Hire me', 'primary--text-btn'))
 
-
-
+    const hambergerMenu = document.querySelector('.hamberger-menu')
+    const closeMobileNav = document.querySelector('.close-mobile-nav')
+    hambergerMenu.addEventListener('click', showNavbar)
+    closeMobileNav.addEventListener('click', hideNavbar)
 }
+
+/*
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+// -------Menu show 
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// ----Menu hidden 
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+*/
+
+
+const showNavbar = () => {
+    const mobileNavbarHidden = document.querySelector('.mobile-navbar-hidden')
+    mobileNavbarHidden.className = 'mobile-navbar'
+}
+const hideNavbar = () => {
+    const mobileNavbarHidden = document.querySelector('.mobile-navbar')
+    mobileNavbarHidden.className = 'mobile-navbar-hidden'
+}
+
 export default mobileNavbar
