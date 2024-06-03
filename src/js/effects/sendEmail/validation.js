2a4d4df9d9c7a554551734085ddf54c9a10d1505
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-06-03 20:58:57 +0430

    feat: set validation to user-name input

--- src/js/effects/sendEmail/validation.js
+++ src/js/effects/sendEmail/validation.js
@@ -1,22 +1,32 @@
+import { checkName } from "./vilidateName.js";
+
 /**
  * loop on inputs and check it by call checkInput function
+ * 1st step: check inputs when user submit the form
+ * 2nd step: check inputs when user keyup
  * @param {HTMLCollection} form - inputs of form
  */
 export const validateForm = (form) => {
     Array.from(form).map(item => {
+        // 1st:
         checkInput(item)
+        // 2nd:
+        item.addEventListener("keyup", () => {
+            checkInput(item)
+        })
     })
 }
 
 /**
  * set condition to finde empty input:
- * set error class to empty inputs and remove error class from empty input
+ * 1 - empty input: set error class 
+ * 2 - user-name input: call checkName function to  the input has user-name class
  * @param {Element} input - input element 
  */
 const checkInput = (input) => {
-    if (input.value === "") {
-        input.classList.add("error")
-    } else {
-        input.classList.remove("error")
-    }
+    // 1 - empty input
+    if (input.value == "") input.classList.add("error")
+    // 2 - user-name input
+    if (input.classList.contains("user-name")) checkName(input)
 }
+
---
+++ src/js/effects/sendEmail/vilidateName.js
@@ -0,0 +1,16 @@
+/**
+ * check user-name input: user can not type number, just able to type English or Farsi
+ * 1st step: create regex for En & Farsi words
+ * 2nd step: check value of input by regex
+ * @param {Element} input input element
+ */
+export const checkName = (input) => {
+    // 1st:
+    const regexName = /^[آ-یA-Za-z\u0600-\u06FF\s,.!؟]+$/;
+    // 2nd:
+    if (!input.value.match(regexName)) {
+        input.classList.add("error", "error-name")
+    } else {
+        input.classList.remove("error", "error-name")
+    }
+}
