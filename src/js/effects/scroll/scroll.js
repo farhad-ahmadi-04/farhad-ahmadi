0a7cb85050784efcc209e73f5d599c156e1ec750
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-06-06 13:37:39 +0430

    fix scroll module

--- src/js/effects/scroll/scroll.js
+++ src/js/effects/scroll/scroll.js
@@ -3,4 +3,8 @@
  * @param {Element} item element for scrool to
  * @returns scroll to targaet element
  */
-export const scroll = (item) => item.scrollIntoView({ behavior: "smooth" })
+export const scroll = (item) => {
+    const mobileNav = document.querySelector('.mobile-navbar');
+    mobileNav.classList.remove("navbar-show")
+    item.scrollIntoView({ behavior: "smooth" })
+}
