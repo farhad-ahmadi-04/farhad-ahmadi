357d08c5eed4e7b36a9baaaa15197b6cc1d8c8df
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-06-08 17:55:24 +0430

    fix problem

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+// rollup.config.js
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+    input: 'src/js/index.js',
+    output: {
+        file: 'dist/bundle.js',
+        format: 'esm',
+        name: 'MyBundle',
+        inlineDynamicImports: true,
+        preserveEntrySignatures: false
+    },
+    plugins: [resolve(), commonjs()]
+};
